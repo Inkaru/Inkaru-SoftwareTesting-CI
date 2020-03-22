c547d0406a5813247c2826a17007e12a141ae94f
Inkaru <pierreant.cab@gmail.com>
2020-03-22 16:28:55 +0100

    changed pom.xml

--- src/test/java/TestCalc.java
+++ src/test/java/TestCalc.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class TestClass {
+public class TestCalc {
 
     private static App calculator;
 
