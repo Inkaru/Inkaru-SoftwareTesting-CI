52f92ae9031ee6a49ed5440f379398cf784f25bf
Antonin <antoninfleury44@gmail.com>
2020-03-24 17:20:41 +0100

    WIP : "Addind test to project"

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,4 +8,20 @@ public class App {
         if(b == 0) throw new ArithmeticException("division by zero is forbidden");
         return a / b;
     }
+
+    public double substract(double a, double b){
+        return 0;
+    }
+    public double multiply(double a, double b){
+        return 0;
+    }
+    public int toInteger(double a){
+        return 0;
+    }
+    public int roundedNumber(double a){
+        return 0;
+    }
+    public int modulo(double a, double b){
+        return 0;
+    }
 }
