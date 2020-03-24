6368c0465524280f1965ab6d5538a62c8943aafc
Antonin <antoninfleury44@gmail.com>
2020-03-24 17:21:50 +0100

    WIP : "Adding all my test to commit"

--- src/test/java/TestCalc.java
+++ src/test/java/TestCalc.java
@@ -30,4 +30,46 @@ public class TestCalc {
         String expectedMessage = "division by zero is forbidden";
         assertEquals(exception.getMessage(), expectedMessage);
     }
+
+    @Test
+    public void substractTest(){ assertEquals(calculator.substract(10.5,5.5), 5.0, 0);}
+
+    @Test
+    public void multiplyTest(){ assertEquals(calculator.multiply(10.5,2.0), 21.0, 0);}
+
+    @Test
+    public void toIntegerTest(){ assertEquals(calculator.toInteger(10.5), 10, 0);}
+
+    @Test
+    public void roundedNumberTest(){
+        assertEquals(calculator.roundedNumber(10.8),11);
+        assertEquals(calculator.roundedNumber(10.3),10);
+        assertEquals(calculator.roundedNumber(10.5),11);//In case of exactly 0.5, we return the upper number
+    }
+
+    @Test
+    public void moduloTest(){
+        assertEquals(calculator.modulo(11,3),2);
+    }
+
+    @Test
+    public void moduloByZeroTest(){
+        Exception exception = assertThrows(ArithmeticException.class, () -> calculator.modulo(5,0));
+
+        String expectedMessage = "modulo by zero is forbidden";
+        assertEquals(exception.getMessage(), expectedMessage);
+    }
+
+    @Test
+    public void moduloWithFloatTest(){
+        Exception exception = assertThrows(ArithmeticException.class, () -> calculator.modulo(5.5,1));
+
+        String expectedMessage = "modulo with float is forbidden";
+        assertEquals(exception.getMessage(), expectedMessage);
+
+        exception = assertThrows(ArithmeticException.class, () -> calculator.modulo(5,1.2));
+        assertEquals(exception.getMessage(), expectedMessage);
+    }
+
+
 }
